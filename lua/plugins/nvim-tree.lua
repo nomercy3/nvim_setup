89d2012bdcab6b09038a2360f62264b60328972f
Vasily <36795610+nomercy3@users.noreply.github.com>
2024-02-09 17:18:39 +0100

    add NvimTree adaptive_size = true

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -10,7 +10,12 @@ return {
 		  'nvim-tree/nvim-web-devicons',
 		},
 		config = function()
-			require('nvim-tree').setup {}
+			require('nvim-tree').setup {
+				view = {
+					adaptive_size = true,
+					side = "left",
+				}
+			}
 		end,
 		keys = {
 			{'<leader>e', '<cmd>NvimTreeToggle<cr>', 'NvimTree [E]xplorer'},
